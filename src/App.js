015c97f31a76291540083195d8b163249548cca3
OspreyDown <jdochoa@madisoncollege.edu>
2025-08-12 20:46:26 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hi I am Jesse and this is my first REACT APP!!!!! :D</h1>
+    </div>
+  );
+}
+
+export default App;
